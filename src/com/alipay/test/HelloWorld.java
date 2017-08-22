ad53cc43dc30308c7768f0b10266796e73e47bee
qian <qian@macpro.local>
2017-08-22 16:30:28 +0800

    Initial commit

---
+++ src/com/alipay/test/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.alipay.test;
+
+/**
+ * Created by qian on 2017/8/22.
+ */
+public class HelloWorld {
+
+  public void test()
+
+  {
+
+  }
+
+
+  @Override
+  public boolean equals(Object obj) {
+    return super.equals(obj);
+  }
+}
---
+++ src/com/alipay/test/i.java
@@ -0,0 +1,8 @@
+package com.alipay.test;
+
+/**
+ * Created by qian on 2017/8/22.
+ */
+public class i {
+
+}
